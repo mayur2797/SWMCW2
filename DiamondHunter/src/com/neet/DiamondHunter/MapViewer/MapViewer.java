e9df07593a24b7b1be8fff18c4643712115b0b07
kesaven8 <kesavenvulliamay@gmail.com>
2017-12-11 16:48:43 +0800

    no changes
    no changes made

--- DiamondHunter/src/com/neet/DiamondHunter/MapViewer/MapViewer.java
+++ DiamondHunter/src/com/neet/DiamondHunter/MapViewer/MapViewer.java
@@ -45,17 +45,12 @@ public class MapViewer implements Initializable {
 		g.drawImage(itemss[1], save_axeY, save_axeX);
 		// Set TextField to default/initial value
 		
-<<<<<<< HEAD
+
 		x_Axe.setText(Integer.toString(save_axeX / 16));
 		y_Axe.setText(Integer.toString(save_axeY / 16));
 		x_Boat.setText(Integer.toString(save_boatX / 16));
 		y_Boat.setText(Integer.toString(save_boatY / 16));
-=======
-		y_Axe.setText(Integer.toString(save_axeX / 16));
-		x_Axe.setText(Integer.toString(save_axeY / 16));
-		y_Boat.setText(Integer.toString(save_boatX / 16));
-		x_Boat.setText(Integer.toString(save_boatY / 16));
->>>>>>> a30f00de7c379dab7028ceb64f85da1ab2ed5818
+
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////
 		// Objects on GUI
@@ -121,70 +116,70 @@ public class MapViewer implements Initializable {
 		});
 	}
 
-//		save.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
-//			@Override
-//			public void handle(javafx.scene.input.MouseEvent event) {
-//				 if(first_boat==false){
-//				try {
-//					if (first_axe == true && first_boat == true) {
-//						checkInvalidPos(axeY, axeX, boatY, boatX);
-//					}
-//					if (first_axe == false && first_boat == false) {
-//						checkInvalidPos(axeX, axeY, boatX, boatY);
-//					}
-//					if (first_axe == true && first_boat == false) {
-//						checkInvalidPos(axeY, axeX, boatX, boatY);
-//					}
-//					if (first_axe == false && first_boat == true) {
-//						checkInvalidPos(axeX, axeY, boatY, boatX);
-//					}
-//
-//					if (first_axe == false) {
-//						save_axeX = axeY * 16;
-//						save_axeY = axeX * 16;
-//					}
-//					if (first_boat == false) {
-//						save_boatX = boatY * 16;
-//						save_boatY = boatX * 16;
-//					}
-//					saved.setContentText("Position of Axe (x,y)  : " + save_axeY / 16 + " " + save_axeX / 16
-//							+ "\nPosition of Boat (x,y) : " + save_boatY / 16 + " " + save_boatX / 16
-//							+ "\n\n * please note that there is possibility that you might not be able to complete the game due to improper positioning.");
-//					saved.showAndWait();
-//				} catch (MyException e) {
-//					Alerts.display("Alert", e.message);
-//				}
-//
-//			}
-//
-//			private void checkInvalidPos(int axeX, int axeY, int boatX, int boatY) throws MyException {
-//				// TODO Auto-generated method stub
-//				if ((axeX * 16) == save_axeX && (axeY * 16) == save_axeY && (boatX * 16) == save_boatX
-//						&& (boatY * 16) == save_boatY) {
-//					throw new MyException("No changes are made. Please use the back button instead!");
-//				}
-//				if (map[axeY][axeX] == 20 || map[axeY][axeX] == 21) {
-//					throw new MyException("Axe cannot be placed onto a tree");
-//				}
-//				if (map[axeY][axeX] == 22) {
-//					throw new MyException("Axe cannot be placed into water");
-//				}
-//				if (map[boatY][boatX] == 20 || map[boatY][boatX] == 21) {
-//					throw new MyException("Boat cannot be placed onto a tree");
-//				}
-//				if (map[boatY][boatX] == 22) {
-//					throw new MyException("Boat cannot be placed into water");
-//				}
-//			}
-//		});
-//	}
+		save.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
+			@Override
+			public void handle(javafx.scene.input.MouseEvent event) {
+				 if(first_boat==false){
+				try {
+					if (first_axe == true && first_boat == true) {
+						checkInvalidPos(axeY, axeX, boatY, boatX);
+					}
+					if (first_axe == false && first_boat == false) {
+						checkInvalidPos(axeX, axeY, boatX, boatY);
+					}
+					if (first_axe == true && first_boat == false) {
+						checkInvalidPos(axeY, axeX, boatX, boatY);
+					}
+					if (first_axe == false && first_boat == true) {
+						checkInvalidPos(axeX, axeY, boatY, boatX);
+					}
 
-	/*@FXML
+					if (first_axe == false) {
+						save_axeX = axeY * 16;
+						save_axeY = axeX * 16;
+					}
+					if (first_boat == false) {
+						save_boatX = boatY * 16;
+						save_boatY = boatX * 16;
+					}
+					saved.setContentText("Position of Axe (x,y)  : " + save_axeY / 16 + " " + save_axeX / 16
+							+ "\nPosition of Boat (x,y) : " + save_boatY / 16 + " " + save_boatX / 16
+							+ "\n\n * please note that there is possibility that you might not be able to complete the game due to improper positioning.");
+					saved.showAndWait();
+				} catch (MyException e) {
+					Alerts.display("Alert", e.message);
+				}
+
+			}
+
+			private void checkInvalidPos(int axeX, int axeY, int boatX, int boatY) throws MyException {
+				// TODO Auto-generated method stub
+				if ((axeX * 16) == save_axeX && (axeY * 16) == save_axeY && (boatX * 16) == save_boatX
+						&& (boatY * 16) == save_boatY) {
+					throw new MyException("No changes are made. Please use the back button instead!");
+				}
+				if (map[axeY][axeX] == 20 || map[axeY][axeX] == 21) {
+					throw new MyException("Axe cannot be placed onto a tree");
+				}
+				if (map[axeY][axeX] == 22) {
+					throw new MyException("Axe cannot be placed into water");
+				}
+				if (map[boatY][boatX] == 20 || map[boatY][boatX] == 21) {
+					throw new MyException("Boat cannot be placed onto a tree");
+				}
+				if (map[boatY][boatX] == 22) {
+					throw new MyException("Boat cannot be placed into water");
+				}
+			}
+		});
+	}
+
+	@FXML
 	public void back() throws Exception {
 		Scene scene = back.getScene();
 		Stage currentscene = (Stage) scene.getWindow();
 		currentscene.hide();
-	}*/
+	}
 
 	//////////////////////////////////////////////////
 	// Methods to extract images from resources
@@ -297,10 +292,7 @@ public class MapViewer implements Initializable {
 	@FXML
 	private TextField x_Axe;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a30f00de7c379dab7028ceb64f85da1ab2ed5818
 	@FXML
 	private Button savebutton;
 
@@ -310,7 +302,7 @@ public class MapViewer implements Initializable {
 	@FXML
 	private Button resetbutton;
 
-<<<<<<< HEAD
+
 	//@FXML
 	//private Button save;
 
@@ -319,20 +311,15 @@ public class MapViewer implements Initializable {
 //	@FXML
 //	//private Button save;
 //
+	//@FXML
+//	private Button back;
+
+
+
+
 	@FXML
 	private Button back;
 
-=======
-<<<<<<< HEAD
-
-
-	@FXML
-	private Button back;
->>>>>>> a30f00de7c379dab7028ceb64f85da1ab2ed5818
-
-
-=======
->>>>>>> 1c534a0c7f765b1c5c051bf5e7ac9740a3984668
 	@FXML
 	private TextField y_Axe;
 
