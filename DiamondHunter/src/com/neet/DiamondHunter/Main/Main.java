4a3435b986fd11b64224d020abc644c75cdf8e6b
mayurgunputh <khcy6mgg@nottingham.edu.my>
2017-12-13 17:23:20 +0800

    wip launch game from mapviewer

--- DiamondHunter/src/com/neet/DiamondHunter/Main/Main.java
+++ DiamondHunter/src/com/neet/DiamondHunter/Main/Main.java
@@ -1,5 +1,8 @@
 package com.neet.DiamondHunter.Main;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
@@ -12,26 +15,49 @@ import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	
+	private static JFrame window;
+	public Stage windowmap;
+	
 	public void start(Stage primaryStage) {
 		try {
+			windowmap = primaryStage;
 			System.out.println("Test!");
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../MapViewer/MapViewer.fxml"));
 			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			windowmap.setScene(scene);
+			windowmap.hide();
 			
-			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent> (){
+			//game
+			window = new JFrame("Diamond Hunter");
+
+			window.add(new GamePanel());
+
+			window.setResizable(false);
+			window.pack();
+
+			window.setLocationRelativeTo(null);
+			window.setVisible(true);
+			window.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
+			
+			
+
+			
+			windowmap.setOnCloseRequest(new EventHandler<WindowEvent> (){
 				public void handle(WindowEvent t){
-					Platform.exit();
+					//Platform.exit();
 					//System.exit(0);
+					windowmap.hide();
 				}
 			});
-			primaryStage.setResizable(false);
+			windowmap.setResizable(false);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	public static JFrame getWindow() {
+		return window;
+	}
 	
 	public static void main(String[] args) {
 		launch(args);
